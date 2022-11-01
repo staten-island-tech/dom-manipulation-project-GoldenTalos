863bde30c424efbad94af1938d08870392878c8e
Sophie Dong <sophiedong2007@gmail.com>
2022-11-01 09:57:47 -0400

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  box: document.getElementById("big-black-box"),
+  points: document.querySelectorAll(".point"),
+};
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.textContent = "This is now a big bigger red box";
+  text.style.fontSize = "40px";
+}
+
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
+
+function changeLi() {
+  DOMSelectors.points.forEach((point) => {
+    console.log(point);
+  });
+}
+changeLi();
