04bf0322593df41f8fc1183e4915b013e5e592bb
Sophie Dong <sophiedong2007@gmail.com>
2022-11-03 09:59:50 -0400

    update

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 DOMSelectors = {
-    enter: document.getElementById("enter"),
-    name: document.getElementById("name"),
-    description: document.getElementById("description"),
-    image: document.getElementById("image"),
-    objects: document.getElementById("objects"),
-}
-DOMSelectors.submit.addEventListener('click', function() {
-    h2 = DOMSelectors.name.value;
-    p = DOMSelectors.description.value;
-    image = DOMSelectors.image.value;
-    DOMSelectors.objects.insertAdjacentHTML(
-        "afterbegin",
-        `<div class="object">
+  enter: document.getElementById("enter"),
+  name: document.getElementById("name"),
+  description: document.getElementById("description"),
+  image: document.getElementById("image"),
+  objects: document.getElementById("objects"),
+};
+DOMSelectors.submit.addEventListener("click", function () {
+  p = DOMSelectors.description.value;
+  h2 = DOMSelectors.name.value;
+  image = DOMSelectors.image.value;
+  DOMSelectors.objects.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="object">
                 <h2>${h2}</h2>
                 <p>${p}</p>
                 <img class="object-image" src="${image}">
                 <br>
                 <button class="remove-button" onclick="this.parentElement.remove()">Remove</button>
         </div>`
-    )
-    
-    DOMSelectors.name.value = '';
-    DOMSelectors.description.value = '';
-    DOMSelectors.image.value = '';
+  );
+
+  DOMSelectors.name.value = "";
+  DOMSelectors.description.value = "";
+  DOMSelectors.image.value = "";
 });
