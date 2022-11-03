09da2193b0d97121c0598f6a872167a61dfc0598
Sophie Dong <sophiedong2007@gmail.com>
2022-11-03 10:16:34 -0400

    update

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 DOMSelectors = {
   enter: document.getElementById("enter"),
-  descriptionInput: document.getElementById("description"),
-  nameInput: document.getElementById("name"),
-  imageInput: document.getElementById("image"),
+  descriptionInput: document.getElementById("description-input"),
+  nameInput: document.getElementById("name-input"),
+  imageInput: document.getElementById("image-input"),
   objects: document.getElementById("objects"),
 };
+
 DOMSelectors.enter.addEventListener("click", function () {
-  p = DOMSelectors.description.value;
-  h2 = DOMSelectors.name.value;
+  descript = DOMSelectors.descriptionInput.value;
+  title = DOMSelectors.nameInput.value;
   image = DOMSelectors.imageInput.value;
   DOMSelectors.objects.insertAdjacentHTML(
     "afterbegin",
     `<div class="object">
-                <h2>${h2}</h2>
-                <p>${p}</p>
+                <h2>${title}</h2>
+                <p>${descript}</p>
                 <img class="object-image" src="${image}">
                 <br>
                 <button class="remove-button" onclick="this.parentElement.remove()">Remove</button>
         </div>`
   );
 
-  DOMSelectors.description.value = "";
-  DOMSelectors.name.value = "";
-  DOMSelectors.image.value = "";
+  DOMSelectors.descriptionInput.value = "";
+  DOMSelectors.nameInput.value = "";
+  DOMSelectors.imageInput.value = "";
 });
