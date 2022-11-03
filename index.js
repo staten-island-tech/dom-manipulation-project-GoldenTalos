d263d9a8371cc763e85f668b411c473416cd6a41
Sophie Dong <sophiedong2007@gmail.com>
2022-11-03 10:18:44 -0400

    update

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ DOMSelectors = {
   descriptionInput: document.getElementById("description-input"),
   nameInput: document.getElementById("name-input"),
   imageInput: document.getElementById("image-input"),
-  objects: document.getElementById("objects"),
+  output: document.getElementById("output"),
 };
 
 DOMSelectors.enter.addEventListener("click", function () {
   descript = DOMSelectors.descriptionInput.value;
   title = DOMSelectors.nameInput.value;
   image = DOMSelectors.imageInput.value;
-  DOMSelectors.objects.insertAdjacentHTML(
+  DOMSelectors.output.insertAdjacentHTML(
     "afterbegin",
-    `<div class="object">
+    `<div class="output">
                 <h2>${title}</h2>
                 <p>${descript}</p>
-                <img class="object-image" src="${image}">
+                <img class="ouput-image" src="${image}">
                 <br>
                 <button class="remove-button" onclick="this.parentElement.remove()">Remove</button>
         </div>`
