3c5f949ecfba4bf60e0ac77aebb1db6de4fb76f3
Sophie Dong <sophiedong2007@gmail.com>
2022-11-17 10:15:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ DOMSelectors = {
   output: document.getElementById("output"),
 };
 
-DOMSelectors.enter.addEventListener("click", function () {
+function resetInput(Input)
+DOMSelectors.enter.addEventListener("submit", function () {
   descript = DOMSelectors.description.value;
   title = DOMSelectors.name.value;
   image = DOMSelectors.image.value;
+  //make object from values
   DOMSelectors.output.insertAdjacentHTML(
     "afterbegin",
     `<div class="output">
@@ -17,11 +19,20 @@ DOMSelectors.enter.addEventListener("click", function () {
                 <p>${descript}</p>
                 <img class="ouput-image" src="${image}">
                 <br>
-                <button class="button" onclick="this.parentElement.remove()">Remove</button>
+                <button class="button">Remove</button>
         </div>`
   );
+
+  const buttons = Array.from(document.getElementsByClassName("remove-button"));
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      this.parentElement.remove();
+    });
+  });
 
   DOMSelectors.description.value = "";
   DOMSelectors.name.value = "";
   DOMSelectors.image.value = "";
+  //queryall remove buttons
+  //addevent listeners to remove buttons with forEAch
 });
